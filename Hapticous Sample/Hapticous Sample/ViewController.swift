1b3eaa8356b98524a6be5c5691d0a981db45ab53
Giovanni Filaferro <g.filaferro@nanotek.it>
2016-09-27 19:28:30 +0200

    Final changes

--- Hapticous Sample/Hapticous Sample/ViewController.swift
+++ Hapticous Sample/Hapticous Sample/ViewController.swift
@@ -13,8 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        Hapticous.shared.triggerSelectionFeedback()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func selectionTriggered(_ sender: AnyObject) {
+        Hapticous.shared.triggerSelectionFeedback()
+    }
+    
+    @IBAction func lightImpactTriggered(_ sender: AnyObject) {
+        Hapticous.shared.triggerImpactFeedback(.light)
+    }
+    @IBAction func mediumImpactTriggered(_ sender: AnyObject) {
+        Hapticous.shared.triggerImpactFeedback(.medium)
+    }
+    @IBAction func highImpactTriggered(_ sender: AnyObject) {
+        Hapticous.shared.triggerImpactFeedback(.heavy)
+    }
+    
+    @IBAction func successNotificationTriggered(_ sender: AnyObject) {
+        Hapticous.shared.triggerNotificationFeedback(.success)
+    }
+    @IBAction func warningNotificationTriggered(_ sender: AnyObject) {
+        Hapticous.shared.triggerNotificationFeedback(.warning)
+    }
+    @IBAction func errorNotificationTriggered(_ sender: AnyObject) {
+        Hapticous.shared.triggerNotificationFeedback(.error)
+    }
+    
+    
 
 }
 
---
+++ README.md
@@ -0,0 +1,9 @@
+Hapticous
+=================
+
+A Swift 3 component to use Haptic feedback on the new iPhones 7 and 7 Plus just like magic.
+Check out the sample project to use it!
+
+<img src="screenshot.png" alt="screenshot" />
+
+MIT License
